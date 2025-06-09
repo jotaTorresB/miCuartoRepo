6c6e0bfcf31982e567745efcc6e689069ca3c98b
Jota <jtorresb@davivienda.com>
2025-06-09 07:36:57 -0500

    metodo de Platzi aghregado

--- API/app.py
+++ API/app.py
@@ -5,12 +5,20 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 @app.get("/familia")
 def get_familia():
     rows = ["Amin", "Marce", "Miranda"]
     return rows
 
+
 @app.get("/superheroesDC")
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+@app.get("/cursosPlatzi")
+def get_cursos():
+    rows = ["Docker", "Bash", "Linux", "Inglés", "Python", "Javascript", "Azure", "AWS"]
+    return rows
+
